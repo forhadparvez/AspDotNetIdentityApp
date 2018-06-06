67ab6890ff83cc5c5407d0b73f362a694b7b28af
Md. Forhad Parvez <forhad.parvez@yahoo.com>
2018-06-06 12:50:37 +0600

    Create Identity Core In Core Library

---
+++ CoreLibrary/IdentityCore/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace CoreLibrary.IdentityCore
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("AspDotNetIdentityDbContext", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
