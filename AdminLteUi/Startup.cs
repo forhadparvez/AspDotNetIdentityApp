e0841bc8d0a50927f68c564d286213879d4d6322
Md. Forhad Parvez <forhad.parvez@yahoo.com>
2018-06-06 14:02:40 +0600

    Add Startup,cs in base  AdminLte Project

---
+++ AdminLteUi/Startup.cs
@@ -0,0 +1,15 @@
+﻿using AdminLteUi;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace AdminLteUi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
